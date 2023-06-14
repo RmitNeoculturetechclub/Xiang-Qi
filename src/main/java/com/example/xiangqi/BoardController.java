7b3d01f973532d88f73ed9368e4ca37eaf69e3a0
Mudoker <huuquoc7603@gmail.com>
2023-06-14 21:11:49 +0700

    ok

---
+++ src/main/java/com/example/xiangqi/BoardController.java
@@ -0,0 +1,60 @@
+package com.example.xiangqi;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class BoardController {
+	public ImageView general_r;
+	public ImageView general_b;
+	public ImageView chariot_b2;
+	public ImageView chariot_b1;
+	public ImageView horse_b1;
+	public ImageView horse_b2;
+	public ImageView elephant_b1;
+	public ImageView elephant_b2;
+	public ImageView canon_r1;
+	public ImageView canon_r2;
+	public ImageView canon_b1;
+	public ImageView canon_b2;
+	public ImageView soldier_b1;
+	public ImageView soldier_b3;
+	public ImageView soldier_b2;
+	public ImageView soldier_b4;
+	public ImageView soldier_b5;
+	public ImageView soldier_r1;
+	public ImageView soldier_r2;
+	public ImageView soldier_r3;
+	public ImageView soldier_r4;
+	public ImageView soldier_r5;
+	public ImageView advisor_b1;
+	public ImageView advisor_b2;
+	public ImageView horse_r1;
+	public ImageView horse_r2;
+	public ImageView elephant_r1;
+	public ImageView elephant_r2;
+	public ImageView advisor_r1;
+	public ImageView advisor_r2;
+	public ImageView chariot_r1;
+	public ImageView chariot_r2;
+
+	public void chariotMove (MouseEvent mouseEvent) {
+	}
+
+	public void generalMove (MouseEvent mouseEvent) {
+	}
+
+	public void horseMove (MouseEvent mouseEvent) {
+	}
+
+	public void elephantMove (MouseEvent mouseEvent) {
+	}
+
+	public void canonMove (MouseEvent mouseEvent) {
+	}
+
+	public void soliderMove (MouseEvent mouseEvent) {
+	}
+
+	public void advisorMove (MouseEvent mouseEvent) {
+	}
+}
