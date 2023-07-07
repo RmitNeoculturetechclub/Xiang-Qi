3cc1c4d664b7df69b2c0b2ca26564d8abd85e8f9
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-07-07 08:51:37 +0700

    Chariot movement

--- JavaFX/src/main/java/com/example/xiangqi/Model/Chariot.java
+++ JavaFX/src/main/java/com/example/xiangqi/Model/Chariot.java
@@ -1,5 +1,6 @@
 package com.example.xiangqi.Model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Chariot extends Piece {
@@ -14,8 +15,52 @@ public class Chariot extends Piece {
 
 	@Override
 	public List<int[]> getAllPossibleMoves(int[] currentPosition, Cell[][] board) {
-		// TODO: Implement this method
-		return super.getAllPossibleMoves(currentPosition, board);
+		List<int[]> possiblePositions = new ArrayList<>();
+		int x = currentPosition[0];
+		int y = currentPosition[1];
+
+		// Check for possible moves horizontally to the right
+		for (int i = x + 1; i < 9; i++) {
+			if (!isValidMove(i, y, board)) {
+				break;
+			}
+			possiblePositions.add(new int[] { i, y });
+		}
+
+		// Check for possible moves horizontally to the left
+		for (int i = x - 1; i >= 0; i--) {
+			if (!isValidMove(i, y, board)) {
+				break;
+			}
+			possiblePositions.add(new int[] { i, y });
+		}
+
+		// Check for possible moves vertically upwards
+		for (int j = y - 1; j >= 0; j--) {
+			if (!isValidMove(x, j, board)) {
+				break;
+			}
+			possiblePositions.add(new int[] { x, j });
+		}
+
+		// Check for possible moves vertically downwards
+		for (int j = y + 1; j < 10; j++) {
+			if (!isValidMove(x, j, board)) {
+				break;
+			}
+			possiblePositions.add(new int[] { x, j });
+		}
+
+		return possiblePositions;
+	}
+
+	private boolean isValidMove(int x, int y, Cell[][] board) {
+		// Check if the position is within the board bounds
+		if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) {
+			return false;
+		}
+
+		return board[x][y].getPiece() == null || board[x][y].getPiece().getPlayerName() != this.getPlayerName();
 	}
 
 	public void setNumPieces(int numPieces) {
